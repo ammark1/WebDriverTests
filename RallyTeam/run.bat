bdc03dd95f6f8cbd7ad0a706e8f44907826b6768
ammark1 <ammark@360logica.com>
2016-08-16 14:30:53 +0530

    Removed run.bat

--- RallyTeam/run.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=F:\Automation\LetsAutomateRally\RallyTeam
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\libs\*
-java org.testng.TestNG F:\RallyTeamWS\RallyTeam\src\com\rallyteam\TestRun\RallyTeamTestSuite.xml
-pause
